81fa062a3dbe86c35981468099f48abeebdc1aef
Sjoerd <s.weerstra@student.fontys>
2018-02-25 17:58:54 +0100

    Save color theme in localStorage

--- portal/script.js
+++ portal/script.js
@@ -2,12 +2,26 @@ const backgrounds = document.querySelectorAll('[data-background]');
 
 backgrounds
     .forEach(el => el.addEventListener('click', function () {
-        document.documentElement.style.setProperty('--color-main', this.dataset.background);
+        const background = this.dataset.background;
+        document.documentElement.style.setProperty('--color-main', background);
+        localStorage.setItem('--color-main', background);
         backgrounds.forEach(background => background.classList.remove('active'));
         this.classList.add('active');
     }));
 
 document.querySelectorAll('[data-color]')
     .forEach(el => el.addEventListener('click', function () {
-        document.documentElement.style.setProperty('--color-tint', this.dataset.color);
+        const color = this.dataset.color;
+        document.documentElement.style.setProperty('--color-tint', color);
+        localStorage.setItem('--color-tint', color);
     }));
+
+document.addEventListener('DOMContentLoaded', () => {
+    Object.entries(localStorage).forEach(([key, value]) => {
+        document.documentElement.style.setProperty(key, value);
+
+        if (key === '--color-main') {
+            document.querySelector(`[data-background='${value}']`).classList.add('active');
+        }
+    });
+});
