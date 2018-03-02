807e6b6072324a377d3b6d207ba49996e9653bb6
Danny Janssen <danny@MacBook-Pro-van-Danny.local>
2018-03-02 09:35:50 +0100

    First commit, project ER

---
+++ verplaatsing/src/main/java/Model/Route.java
@@ -0,0 +1,18 @@
+package Model;
+
+import Support.Point;
+import java.util.ArrayList;
+
+public class Route {
+    private int carId;
+    private ArrayList<Point> points;
+
+    public Route(){
+
+    }
+
+    //region getters and setters
+
+
+    //endregion
+}
---
+++ verplaatsing/src/main/java/Model/Vehicle.java
@@ -0,0 +1,14 @@
+package Model;
+
+public class Vehicle {
+
+    private long AUTHORISATION_ID;
+    private int vehicleId;
+    private int serial;
+    private String licensePlate;
+
+    public Vehicle(String licensePlate){
+        this.licensePlate = licensePlate;
+    }
+
+}
