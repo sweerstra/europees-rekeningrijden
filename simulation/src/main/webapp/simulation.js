36c88efe6043098e9be2309cceba431853762c1f
Sjoerd <s.weerstra@student.fontys>
2018-03-21 23:02:22 +0100

    Simulate based on selected or generated coordinates

--- simulation/src/main/webapp/simulation.js
+++ simulation/src/main/webapp/simulation.js
@@ -1,77 +1,164 @@
-const directionsDisplay = new google.maps.DirectionsRenderer({ draggable: true });
 const directionsService = new google.maps.DirectionsService();
+
+const fromInput = document.getElementById('from');
+const toInput = document.getElementById('to');
+const speedInput = document.getElementById('speed');
+const simulationToggleButton = document.getElementById('simulation-toggle');
+const generateRouteButton = document.getElementById('generate-route');
+
+const MOVEMENT_API_URL = 'localhost:8080/movement/api/movement';
+const SIMULATION_API_URL = 'localhost:8080/simulation/api/generate';
+
+const defaultStrokeColor = localStorage.getItem('--color-main') || '#4CAF50';
+
+const ROUTES = [
+    { from: '51.538433, -0.14467214', to: '51.527092, -0.14466009' },
+    { from: '51.525867, -0.13448699', to: '51.529404, -0.14631915' },
+    { from: '51.533173, -0.14858663', to: '51.538433, -0.14467214' }
+];
+
+let trackerId = 1;
 let map;
-let line;
 
 document.addEventListener('DOMContentLoaded', () => {
-    const myOptions = {
+    const mapOptions = {
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
         center: new google.maps.LatLng(51.509865, -0.118092)
     };
 
-    map = new google.maps.Map(document.getElementById("google-map"), myOptions);
-    directionsDisplay.setMap(map);
-    // directionsDisplay.setPanel(document.getElementById("directions"));
+    map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
 
-    const simulationToggle = document.getElementById('simulation-toggle');
+    simulationToggleButton.addEventListener('click', function () {
+        if (!this.classList.contains('playing')) {
+            // start simulation
+            calcRoute(fromInput.value, toInput.value);
+        } else {
+            // stop simulation
+        }
 
-    simulationToggle.addEventListener('click', function () {
         this.classList.toggle('playing');
-        calcRoute();
+    });
+
+    generateRouteButton.addEventListener('click', function () {
+        const { value } = speedInput;
+
+        if (value) {
+            speedInput.classList.remove('warning');
+            // get two coordinates from simulation API based on speed value
+            /* post(SIMULATION_API_URL, { speed: parseFloat(value) })
+                .then(resp => resp); */
+
+            const randomIndex = Math.floor(Math.random() * ROUTES.length);
+            const randomRoute = ROUTES[randomIndex];
+            ROUTES.splice(randomIndex, 1);
+            calcRoute(randomRoute.from, randomRoute.to);
+        } else {
+            speedInput.classList.add('warning');
+            console.warn('Please enter a speed value.');
+        }
+    });
+
+    // load theme colors from localStorage
+    Object.entries(localStorage).forEach(([key, value]) => {
+        document.documentElement.style.setProperty(key, value);
     });
 });
 
-function calcRoute() {
-    const request = {
-        origin: document.getElementById('from').value,
-        destination: document.getElementById('to').value,
+function calcRoute(from, to) {
+    if (!from || !to) {
+        console.warn('From or to coordinates were null or undefined.');
+        return;
+    }
+
+    const requestOptions = {
+        origin: from,
+        destination: to,
         travelMode: 'DRIVING'
     };
 
-    directionsService.route(request, function (response, status) {
-        const steps = response.routes[0].overview_path;
-        const [first] = steps;
-
-        console.log(first.lat());
-        console.log(first.lng());
-
+    directionsService.route(requestOptions, (response, status) => {
         if (status === google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-            //document.getElementById('Gresponse').innerHTML = JSON.stringify(response);
-            createPolyline(response);
+            const polyline = createPolyline(response);
+            const [route] = response.routes;
+            addSimulation(route, polyline);
         }
     });
 }
 
+function addSimulation(route, polyline) {
+    const path = route.overview_path;
+    const { length } = path;
+
+    let movementCount = 1;
+
+    const interval = setInterval(() => {
+        if (movementCount <= length) {
+            animateStep(polyline, movementCount, length);
+
+            const { lat, lng } = path[movementCount - 1];
+            const { trackerId, movementId, time } = polyline;
+
+            const routeResult = {
+                latitude: lat(),
+                longitude: lng(),
+                trackerId, movementId, time
+            };
+
+            console.log(routeResult);
+
+            // make post request to save movement
+            /* post(MOVEMENT_API_URL, routeResult)
+                .catch(err => {
+                    console.error(err.message);
+                    clearInterval(interval);
+                }); */
+        } else {
+            clearInterval(interval);
+        }
+        movementCount++;
+    }, 1000);
+}
+
+function post(url, data) {
+    const options = {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+    };
+
+    return fetch(url, options)
+        .then(resp => resp.json());
+}
+
 function createPolyline(directionResult) {
-    line = new google.maps.Polyline({
+    const line = new google.maps.Polyline({
         path: directionResult.routes[0].overview_path,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.5,
+        strokeColor: '#F44336',
+        strokeOpacity: 0.4,
         strokeWeight: 4,
         icons: [{
             icon: {
                 path: google.maps.SymbolPath.CIRCLE,
-                scale: 8,
-                strokeColor: '#393'
+                scale: 10,
+                strokeColor: defaultStrokeColor,
+                strokeOpacity: 1
             },
-            offset: '100%'
-        }]
+            offset: '0%'
+        }],
+        trackerId: trackerId++,
+        time: new Date()
     });
+
+    line.setVisible(true);
     line.setMap(map);
-    animate();
+    return line;
 }
 
-function animate() {
-    let count = 0;
-    window.setInterval(function () {
-        count = (count + 1) % 200;
-
-        const icons = line.get('icons');
-        icons[0].offset = (count / 2) + '%';
-        line.set('icons', icons);
-    }, 20);
+function animateStep(polyline, count, pathLength) {
+    const icons = polyline.get('icons');
+    icons[0].offset = ((count / pathLength) * 100) + '%';
+    polyline.set('icons', icons);
+    polyline.movementId = count;
 }
 
-// google.maps.event.addDomListener(window, 'load', initialize);
