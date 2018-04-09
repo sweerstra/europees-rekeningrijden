21077ca95a7c84da38addaf0768be446b745fbc0
Sinclair Rambelje <sinne_pin@hotmail.com>
2018-04-09 15:11:38 +0200

    Integrate JWT token authentication

--- government/src/main/controllers/JAXRSApplication.java
+++
@@ -1,8 +0,0 @@
-package main.controllers;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-//
-@ApplicationPath("/api")
-public class JAXRSApplication extends Application {
-
-}
--- government/src/main/java/dao/EmployeeDao.java
+++ government/src/main/java/dao/EmployeeDao.java
@@ -15,4 +15,13 @@ public class EmployeeDao extends DaoFacade<Employee> implements IEmployeeDao{
         super(Employee.class);
     }
 
+    public Employee getEmployeeByEmail(String email) {
+        try {
+            return (Employee) em.createQuery("SELECT u FROM Employee u WHERE u.email = :email")
+                    .setParameter("email", email)
+                    .getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
